a42aadae968eaf87a15a086fe9ea8dfd44b59ebf
Hansky Mulya <me@hansky.dev>
2024-10-20 17:12:13 +0700

    chore: move fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,9 @@
 import type { Metadata, Viewport } from 'next';
-import localFont from 'next/font/local';
 
 import { siteConfig } from '@/config/site';
+import { geistMono, geistSans } from '@/lib/fonts';
 
 import '@/styles/globals.css';
-
-const geistSans = localFont({
-  src: './fonts/GeistVF.woff',
-  variable: '--font-geist-sans',
-  weight: '100 900',
-});
-const geistMono = localFont({
-  src: './fonts/GeistMonoVF.woff',
-  variable: '--font-geist-mono',
-  weight: '100 900',
-});
 
 export const metadata: Metadata = {
   title: {
